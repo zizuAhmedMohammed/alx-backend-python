71a3e9594b1583b5568fa59631b45d73adcbf01a
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-03-30 20:57:02 +0000

    6-sum_mixed_list.py

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module 6-sum_mixed_list.py"""
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
+    """Takes a mixed list of integers"""
+    return sum(input_list)
