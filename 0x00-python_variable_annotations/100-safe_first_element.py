18f64ddcdb60a97d50733362d20e9c940b623da6
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-03-30 21:03:14 +0000

    100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
-safe_first_element = __import__('100-safe_first_element').safe_first_element
+from typing import TypeVar, Mapping, Any, Union
 
-print(safe_first_element.__annotations__)
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
