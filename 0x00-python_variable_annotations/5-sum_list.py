f8bc7f60e851d1747a9a3040e7506bcc7dfca87d
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-03-30 20:56:32 +0000

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module 5-sum_list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Takes a list of floats"""
+    return sum(input_list)
