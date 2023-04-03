259ec79a5175af3d27b4887b8cbaba76c5863630
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-03 20:34:08 +0000

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" An asynchronous function `wait_n` that waits for multiple random delay tasks to complete. """
+"""An asynchronous function `wait_n` that waits for multiple random delay tasks to complete"""
 
 import asyncio
 from typing import List
@@ -10,9 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Wait for `n` tasks with maximum delay
-    
     of `max_delay` seconds
     """
-    
+
     tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
