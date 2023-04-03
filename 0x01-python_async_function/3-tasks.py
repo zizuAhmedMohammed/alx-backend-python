b356b637dc5439c717150e80cf14fcc0b5f887aa
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-03 21:22:20 +0000

    3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""do not create an async function, use the regular function syntax"""
+"""do not create an async function, use the regular function """
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
