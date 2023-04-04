2b0905302bc0639da0ad983bb807c3d296af9234
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-04 19:39:55 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,4 +12,4 @@ async def async_generator() -> Genrator[float, None, None]:
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
