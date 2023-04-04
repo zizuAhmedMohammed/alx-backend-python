ed92eeee2d306d23d264b913a23ad7b4bc6aec2a
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-04 18:37:22 +0000

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-
+"""A coroutine called async_comprehension that takes no arguments"""
 import asyncio
-import random
+
 
 async def async_generator():
+    """
+    coroutine will collect 10 random numbers using
+    an async comprehensing over async_generator
+    """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
 
+
 async def async_comprehension():
+    """return the 10 random numbers"""
     random_numbers = [number async for number in async_generator()]
     return random_numbers
