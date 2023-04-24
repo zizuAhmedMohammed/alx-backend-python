422e9facfcfc44c4b3dd7dc837922410bae2bb24
Abdulaziz Ahmed <azizazu0934@gmail.com>
2023-04-24 19:34:59 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,8 @@ Parameterize a unit test
 """
 import unittest
 from typing import Mapping, Sequence, Any
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
